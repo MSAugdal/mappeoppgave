8cee0efcd50b3f2c32262e35e12ef62225b4103c
Mathias Schjølberg Augdal <mathias.s.augdal@gmail.com>
2022-02-04 15:31:05 +0100

    la til kommentarer

--- oppgave1.py
+++ oppgave1.py
@@ -1,40 +1,48 @@
 
 from turtle import *
- 
-import turtle 
-  
+
+import turtle
+
+
 def kube():
     # lager et område for turtle å rendere #
     # lager tittel til vinduet
-    # setter bakgrunnsfarge til svart og penn farge til blå
+    # setter bakgrunnsfarge til grønn og pennfarge til svart
     # lager variabler "turt" for å kalle turtle med kortere navn
     vindu = turtle.Screen()
-    vindu.bgcolor("black")
+    vindu.bgcolor("green")
     vindu.title("3D KUBE")
     turt = turtle.Turtle()
-    turt.color("blue")           
+    turt.color("black")
 
-    #lager første firkant
+    # lager første firkant
     for i in range(4):
         turt.forward(100)
         turt.left(90)
 
-    #lager første linje som kobler første firkant med firkant 2
-    turt.goto(50,50)
-    
-    # for i in range(4):
-    #     turt.forward(100)
-    #     turt.left(90)
+    # lager linje i nederst venstre hjørne som kobler de to firkantene sammmen
+    turt.goto(50, 50)
 
-    # turt.goto(150,50)
-    # turt.goto(100,0)
-    
-    # turt.goto(100,100)
-    # turt.goto(150,150)
-    
-    # turt.goto(50,150)
-    # turt.goto(0,100)
+    # lager firkant 2
+    for i in range(4):
+        turt.forward(100)
+        turt.left(90)
 
+    # lager linje i nederst høyre hjørne som kobler de to firkantene sammen
+    turt.goto(150, 50)
+    turt.goto(100, 0)
+
+    # lager linje i øvers høyre hjørne som kobler de to firkantene sammen
+    turt.goto(100, 100)
+    turt.goto(150, 150)
+
+    # lager linje i øverst ventre hjørne som kobler de to firkantene sammen
+    turt.goto(50, 150)
+    turt.goto(0, 100)
+
+    # starter eventloop
+    # gjør akkurat det samme som tkinter.mainloop()
     turtle.done()
+
 
 kube()
