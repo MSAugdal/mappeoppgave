f582df14af3512f4cfa73c9127d7cb58f3a712e7
Mathias Schjølberg Augdal <mathias.s.augdal@gmail.com>
2022-02-04 17:49:14 +0100

    added text

---
+++ oppgave2/grunderen.py
@@ -0,0 +1,15 @@
+'''
+Velkommen til bedrift, 
+vi selger varer til kr stykket + x kr frakt per passe (minimum x pakker per kjøp). 
+Om du kjøper mer enn x pakker vil du få en rabatt på x%, og om du kjøper mer enn y pakker gir vi deg y%. 
+Om prisen overstiger z kroner koster det ingenting for frakt.
+	
+Hvor mange pakker ønsker du å bestille? Skriv her: 
+
+Prisen blir xyz kroner, 
+zxy kroner er trukket fra i rabatt
+kroner i frakt
+	
+Ønsker du å gjøre en ny prisutregning? J for ja eller N for nei. Skriv her: N
+	
+Ta kontakt på telefon 555 44 555 for å gjennomføre handelen. Takk for nå'''
