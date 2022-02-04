9a742fa5741695f63854e6eb0cff1595e60ba5aa
Mathias Schjølberg Augdal <72888306+MSAugdal@users.noreply.github.com>
2022-02-04 14:45:46 +0100

    laget kode

--- oppgave1.py
+++ oppgave1.py
@@ -1,13 +1,40 @@
+
 from turtle import *
-from tkinter import mainloop
+ 
+import turtle 
+  
+def kube():
+    # lager et område for turtle å rendere #
+    # lager tittel til vinduet
+    # setter bakgrunnsfarge til svart og penn farge til blå
+    # lager variabler "turt" for å kalle turtle med kortere navn
+    vindu = turtle.Screen()
+    vindu.bgcolor("black")
+    vindu.title("3D KUBE")
+    turt = turtle.Turtle()
+    turt.color("blue")           
 
-#A:
-speed(100)
-def firkant():
-    for n in range(4):
-        forward(200)
-        left(90)
+    #lager første firkant
+    for i in range(4):
+        turt.forward(100)
+        turt.left(90)
 
-firkant()
+    #lager første linje som kobler første firkant med firkant 2
+    turt.goto(50,50)
+    
+    # for i in range(4):
+    #     turt.forward(100)
+    #     turt.left(90)
 
-mainloop()
+    # turt.goto(150,50)
+    # turt.goto(100,0)
+    
+    # turt.goto(100,100)
+    # turt.goto(150,150)
+    
+    # turt.goto(50,150)
+    # turt.goto(0,100)
+
+    turtle.done()
+
+kube()
